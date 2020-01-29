4fdeab8875661a42508de77a18e31f2d73d371e1
dianlujitao <dianlujitao@lineageos.org>
2020-01-29 22:45:42 +0800

    whyred: lights: red -> white
    Change-Id: I097c542f95434d85b8c041b156dfd81952c64164

--- light/Light.cpp
+++ light/Light.cpp
@@ -22,11 +22,9 @@
 
 #include <fstream>
 
-#define LEDS            "/sys/class/leds/"
-
-#define LCD_LED         LEDS "lcd-backlight/"
-#define RED_LED       LEDS "red/"
-
+#define LEDS                       "/sys/class/leds/"
+#define LCD_LED                    LEDS "lcd-backlight/"
+#define WHITE                      LEDS "white/"
 #define BLINK           "blink"
 #define BRIGHTNESS      "brightness"
 #define DUTY_PCTS       "duty_pcts"
